1fc26810bbd75b50bac6548ba89a6f24ca22393e
Nqsir <steyaert.victor@gmail.com>
2019-09-13 08:38:28 +0200

    [Progress] [Progress] <First parsing from yesterday>

---
+++ Expert_system.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+import re
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='py Expert_system.py')
+    parser.add_argument('text_file', help='A text file with instructions')
+    args = parser.parse_args()
+
+    file = os.path.join(os.getcwd(), args.text_file)
+
+    if os.path.exists(file)and os.path.isfile(file) and file.endswith('.txt'):
+        try:
+            rules = []
+            facts = []
+            queries = []
+            extracted_file = []
+            with open(file, 'r') as in_file:
+                for line in in_file:
+                    if not line.startswith('#') and line != '\n':
+                        if '#' in line:
+                            rest = line.split('#')
+                            extracted_file.append(rest[0])
+                        else:
+                            extracted_file.append(line)
+
+            # DEBUG
+            for ext in extracted_file:
+                print(ext)
+
+
+
+        except PermissionError:
+            print('Permission error, failed opening the file')
+    else:
+        print(f'The selected file must be a text file i.e. with extension ".txt"\n')
