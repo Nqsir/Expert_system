77be04b42c90d75dbe08727118884b0bb765be5e
freddy pupier <34068994+pups-enterprise@users.noreply.github.com>
2019-09-16 21:37:03 +0200

    [Test][Progress] [Test]<update get_list.py> [Progress]<fonction translat_to_list OK>

--- get_list.py
+++ get_list.py
@@ -1,42 +1,111 @@
 CONST_CHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-var = ["(!A+B+!C)=>Z", "(D+(!E+F))=>A", "(D|F)=>C", "(G+H)=>Z", "(F)=>D", "(D+G)=>X", "(A|J+S)=>Z"]
+def simply_list(list):
+    list_if = list[0]
+    list_then = list[1]
+
+    x = 0
+    while x < len(list_then):
+        y = x+1
+        while y < len(list_then):
+            if list_then[x] == list_then[y]:
+                list_start = ["|"]
+                list_if[x].append(list_start)
+                list_if[x].extend(list_if[y])
+                del list_if[y]
+                del list_then[y]
+                y = y-1
+            y = y + 1
+        x = x + 1
+
+    x = 0
+    flag = 1
+    while flag > 0:
+        flag = 0
+        x = -1
+        for elem_A in list_then:
+            x = x + 1
+            y = -1
+            for line in list_if:
+                for n, elem_B in enumerate(line):
+                    y = y + 1
+                    if elem_B == elem_A[0]:
+                        line.pop(n)
+                        for o, elt in enumerate(list_if[x]):
+                            line.insert(n + o, elt)
+                        flag = flag + 1
+
+
+def to_list(list_if):
+    tmp = []
+    for elt in list_if:
+        list_tmp_if = []
+        for n, char in enumerate(elt):
+            list_tmp = []
+            if n > 0 and (char in CONST_CHAR) and elt[n - 1] == '!':
+                list_tmp.append('!'+elt[n])
+            elif char != '!':
+                list_tmp.append(elt[n])
+            if len(list_tmp):
+                list_tmp_if.append(list_tmp)
+        tmp.append(list_tmp_if)
+    return tmp
+
+
+def convert_to_only_one_then(list):
+    list_and = ["+"]
+    for n, line in enumerate(list[1]):
+        x = 0
+        while x < len(line):
+            if list_and == list[1][n][x]:
+                list[0].append(list[0][n].copy())
+                list_tmp = []
+                list_tmp.append(list[1][n][x+1])
+                list[1].append(list_tmp.copy())
+                del list[1][n][x]
+                del list[1][n][x]
+                x = x - 1
+            x = x + 1
+
+
+def translat_to_list(list):
+    list_if = []
+    list_then = []
+    for n, elt in enumerate(list):
+        list_if.append(elt[0])
+        list_then.append(elt[1])
+    list.clear()
+    list.append(to_list(list_if))
+    list.append(to_list(list_then))
+    convert_to_only_one_then(list)
+    simply_list(list)
+
+
+#
+# utilise une liste double en entrer : liste[[if],[then]]
+#
+
+
+var = ["(A+!B+C)=>Z+!V", "(!G+!H)=>A+Z", "(A|J+S)=>Z"]
 
 list_total = []
-list_if = []
-list_then = []
 
 for elt in var:
     list_total.append(elt.split("=>"))
 
+print("BEFOR :")
+print("\tIF :")
 for elt in list_total:
-    print(elt)
-
+    print("\t\t", elt[0])
+print("\tTHEN :")
 for elt in list_total:
-    list_tmp_if = []
+    print("\t\t", elt[1])
 
-    for n, char in enumerate(elt[0]):
-        list_tmp = []
-        if n > 0 and (char in CONST_CHAR) and elt[0][n-1] == '!':
-            list_tmp.append('!')
-            list_tmp.append(elt[0][n])
-        elif char != '!':
-            list_tmp.append(elt[0][n])
-        if len(list_tmp):
-            list_tmp_if.append(list_tmp)
-    list_if.append(list_tmp_if)
-
-    list_tmp_2 = []
-    if '!' in elt[1]:
-        list_tmp_2.append('!')
-    else:
-        list_tmp_2.append('')
-    list_tmp_2.append(elt[1].replace('!', ''))
-    list_then.append(list_tmp_2)
-
-for elt in list_then:
-    print(elt)
-
-
-for elt in list_if:
-    print(elt)
+print("AFTER :")
+translat_to_list(list_total)
+print("\tIF :")
+for elt in list_total[0]:
+    print("\t\t", elt)
+print("\tTHEN :")
+for elt in list_total[1]:
+    print("\t\t", elt)
