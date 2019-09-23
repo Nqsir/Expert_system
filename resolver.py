1330db6a0160034ff4bd5117749f4ebfc6be852e
freddy pupier <34068994+pups-enterprise@users.noreply.github.com>
2019-09-23 17:24:41 +0200

    [Progress] [Progress]<resolver.py>

--- resolver.py
+++ resolver.py
@@ -1,18 +1,26 @@
 CONST_CHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 CONST_NOT = "!"
-
-
+CONST_REGEX_UNITARY = ""
+CONST_REGEX_GROUP = ""
+CONST_REGEX_PAIR_AND = ""
+CONST_REGEX_PAIR_OR = ""
+CONST_REGEX_PAIR_XOR = ""
 
 
 class Resolver:
-    def __init__(self, list_string):
+    def __init__(self, list_string, list_fact):
         self.list_string = list_string
 
+    # ------------------------------------------------------------------------------------------------------------------
+    #   methode resolution
+    # ------------------------------------------------------------------------------------------------------------------
     def order(self, line):
-        memorie_string = ""
+        memory_string_inc = ""
+
         # recherche pattern avec la regex unitaire
-        # tuple_regex_unitary = search_regex("str", "regex_utinary")
-        tuple_regex_unitary = []
+        tuple_regex_unitary = self.search_regex(line, CONST_REGEX_UNITARY)
+
+        # boucle remplacement des lettre par leur valeur
         for n, element_unitary in enumerate(tuple_regex_unitary):
             if CONST_NOT in element_unitary:
                 tmp_str = self.calc_neg(element_unitary)
@@ -23,27 +31,27 @@ class Resolver:
             else:
                 line.replace(element_unitary, tmp_str)
 
-        # boucle principale
-        flag_stop = 1
-        while flag_stop == 1:
-            flag_stop = 0
+        # boucle principale de resolution
+        flag_run = 1
+        while flag_run == 1:
+            line_copy = line.copy()
 
             # recherche pattern avec la reg regex de groupe
-            tuple_regex_groupe = self.search_regex("str", "regex_group")
+            tuple_regex_groupe = self.search_regex(line, CONST_REGEX_GROUP)
+
             # boucle des goupe
             flag_regex_group = 1
             while flag_regex_group == 1:
-
-                line_copy = line.copy()
-
+                group_copy = line.copy()
                 for m, group in enumerate(tuple_regex_groupe):
+
                     # boucle de toute les occurrence AND
                     flag_regex_pair_and = 1
                     while flag_regex_pair_and == 1:
                         # creation d'une copie de la regle
-                        tmp_line_copy = line.copy()
+                        tmp_and_copy = line.copy()
                         # recherche pattern avec la regex paire and
-                        tuple_regex_pair_and = self.search_regex(group[m], "regex_pair_and")
+                        tuple_regex_pair_and = self.search_regex(group[m], CONST_REGEX_PAIR_AND)
                         for n, pair_and in enumerate(tuple_regex_pair_and):
                             # fonction qui recupere que les 2 param
                             # recup_param(pair_and, param_1, param_2)
@@ -51,16 +59,16 @@ class Resolver:
                             param_2 = ""
                             rep = self.calc_and(param_1, param_2)
                             str(line).replace(tuple_regex_pair_and[n], rep)
-                        if tmp_line_copy == line:
+                        if tmp_and_copy == line:
                             flag_regex_pair_and = 0
 
                     # boucle de toute les occurrence OR
                     flag_regex_pair_or = 1
                     while flag_regex_pair_or == 1:
                         # creation d'une copie de la regle
-                        tmp_line_copy = line.copy()
+                        tmp_or_copy = line.copy()
                         # recherche pattern avec la regex paire or
-                        tuple_regex_pair_or = self.search_regex(group[m], "regex_pair_or")
+                        tuple_regex_pair_or = self.search_regex(group[m], CONST_REGEX_PAIR_OR)
                         for n, pair_or in enumerate(tuple_regex_pair_or):
                             # fonction qui recupere que les 2 param
                             # recup_param(pair_or, param_1, param_2)
@@ -68,7 +76,7 @@ class Resolver:
                             param_2 = ""
                             rep = self.calc_or(param_1, param_2)
                             str(line).replace(tuple_regex_pair_or[n], rep)
-                        if tmp_line_copy == line:
+                        if tmp_or_copy == line:
                             flag_regex_pair_or = 0
 
                     # boucle de toute les occurrence XOR
@@ -77,7 +85,7 @@ class Resolver:
                         # creation d'une copie de la regle
                         tmp_line_copy = line.copy()
                         # recherche pattern avec la regex paire xor
-                        tuple_regex_pair_xor = self.search_regex(group[m], "regex_pair_xor")
+                        tuple_regex_pair_xor = self.search_regex(group[m], CONST_REGEX_PAIR_XOR)
                         for n, pair_or in enumerate(tuple_regex_pair_xor):
                             # fonction qui recupere que les 2 param
                             # recup_param(pair_or, param_1, param_2)
@@ -90,7 +98,9 @@ class Resolver:
                 if line_copy == line:
                     flag_regex_group = 0
 
-
+    # ------------------------------------------------------------------------------------------------------------------
+    #   methode de calcul AND
+    # ------------------------------------------------------------------------------------------------------------------
     def calc_and(self, param_1, param_2):
         tmp = ""
         if (CONST_NOT in param_1 or param_1 in CONST_CHAR) and (CONST_NOT in param_2 or param_2 in CONST_CHAR):
@@ -117,6 +127,9 @@ class Resolver:
                 tmp = "0"
         return tmp
 
+    # ------------------------------------------------------------------------------------------------------------------
+    #   methode de calcul OR
+    # ------------------------------------------------------------------------------------------------------------------
     def calc_or(self, param_1, param_2):
         tmp = ""
         if (CONST_NOT in param_1 or param_1 in CONST_CHAR) and (CONST_NOT in param_2 or param_2 in CONST_CHAR):
@@ -143,6 +156,9 @@ class Resolver:
                 tmp = "0"
         return tmp
 
+    # ------------------------------------------------------------------------------------------------------------------
+    #   methode de calcul XOR
+    # ------------------------------------------------------------------------------------------------------------------
     def calc_xor(self, param_1, param_2):
         tmp = ""
         if (CONST_NOT in param_1 or param_1 in CONST_CHAR) or (CONST_NOT in param_2 or param_2 in CONST_CHAR):
@@ -159,16 +175,25 @@ class Resolver:
                 tmp = "0"
         return tmp
 
+    # ------------------------------------------------------------------------------------------------------------------
+    #   methode de remplacement de la valeur negative
+    # ------------------------------------------------------------------------------------------------------------------
     def calc_neg(self, param_1):
         tmp = ""
         # remplace la letre par l'inverse de sa valeur si elle est connu, sinon laisse comme c'est
         return tmp
 
+    # ------------------------------------------------------------------------------------------------------------------
+    #   methode de remplacement de la valeur positive
+    # ------------------------------------------------------------------------------------------------------------------
     def calc_pos(self, param_1):
         tmp = ""
         # remplace la letre par sa valeur si elle est connu, sinon laisse comme c'est
         return tmp
 
+    # ------------------------------------------------------------------------------------------------------------------
+    #   methode de recherche regex dans une string
+    # ------------------------------------------------------------------------------------------------------------------
     def search_regex(self, string, regex):
 
         tuple_regex = list(set())
